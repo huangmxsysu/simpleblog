ac75e8b89a74fdac914d7620f97545acc19335c1
mosen <995304572@qq.com>
2017-03-05 17:52:43 +0800

    增加了评论模块

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -36,7 +36,7 @@ from django.db.models import Count
 
 
 
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
@@ -88,6 +88,34 @@ def post_remove(request, pk):
     return redirect('blog.views.post_list')
 
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+
+
+from .models import Comment
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
 
 
