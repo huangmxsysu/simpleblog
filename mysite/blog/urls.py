14a1bb2e6d4ce1a536f541167ec4c865eb601cd2
mosen <995304572@qq.com>
2017-03-05 12:31:59 +0800

    增加了新增文章编辑修改文章form表单形式

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    # url(r'^post/new2/$', views.post_new, name='post_new2'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$',views.post_edit,name='post_edit'),
 ]
